ee9af8cbbac6f63d58c0cc9e49d2c04c4e01c7e4
nishilrbalmoori <nishil.r.balmoori@gmail.com>
2026-07-21 11:32:57 -0500

    Player Switching + Camera Follow

--- UnityProjSoccer/Assets/Scripts/PlayerController.cs
+++ UnityProjSoccer/Assets/Scripts/PlayerController.cs
@@ -3,19 +3,24 @@ public class PlayerController : MonoBehaviour
 {
 
     public bool isActive = false;
+    public float mouse_sens = 100f;
 
     private Rigidbody rb;
     private Vector3 movement_activation;
-
+    private float xRot = 0f;
     
     [SerializeField]
     private float speed = 5.0f;
 
+
     void Start(){
         rb = this.GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update(){
+
+
         movement_activation.x = Input.GetAxis("Horizontal");
         movement_activation.z = Input.GetAxis("Vertical");
     }
--- UnityProjSoccer/Assets/Scripts/TeamController.cs
+++ UnityProjSoccer/Assets/Scripts/TeamController.cs
@@ -1,46 +1,76 @@
 using UnityEngine;
+using Unity.Cinemachine;
 using System.Collections.Generic;
 
 public class TeamController : MonoBehaviour
 {
+    public CinemachineCamera camera;
     private List<GameObject> players = new List<GameObject>();
     void Start()
     {        
+
+        //Init Players In A Team
         foreach(Transform child in transform){
             if (child.TryGetComponent<PlayerController>(out PlayerController script)){
-                players.add(child);
+                players.Add(child.gameObject);
             }
         }
+
+        FollowActivePlayer();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Enter)){
+        SwitchActivePlayer();
+    }
+
+    private void SwitchActivePlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
             GameObject active = null;
-            Dictionary<GameObject, float> dists = new Dictionary<string, float>();
+            Dictionary<GameObject, float> dists = new Dictionary<GameObject, float>();
 
 
             foreach(GameObject player in players){
-                if (player.GetComponent<PlayerController>().isActive()) active = player;
+                if (player.GetComponent<PlayerController>().isActive) active = player;
             }            
 
             foreach(GameObject player in players)
             {
-                if(player.GetInstanceId() != active.GetInstanceId())
+                if(player.GetEntityId().GetHashCode() != active.GetEntityId().GetHashCode())
                 {
                     dists.Add(player, Vector3.Distance(active.transform.position, player.transform.position));
                 }
             }
 
-            GameObject min_id = null;
+            GameObject min = null;
 
             foreach(var(id, dist) in dists)
             {
-                if(min_id == null || dists[id] < dists[min_id]) min_id = id;
+                if(min == null || dists[id] < dists[min]) min = id;
             }
 
-            active.GetComponent<PlayerController>.isActive = false;
-            min_id.GetComponent<PlayerController>.isActive = true;
+            PlayerController active_2_un = active.GetComponent<PlayerController>();
+            PlayerController un_2_active = min.GetComponent<PlayerController>();
+
+            active_2_un.isActive = false;
+            un_2_active.isActive = true;
+
+            FollowActivePlayer();
+        }
+    }
+
+        private void FollowActivePlayer()
+    {
+        foreach(GameObject player in players)
+        {
+            if (player.TryGetComponent<PlayerController>(out PlayerController script)){
+                if (script.isActive)
+                {
+                    camera.Target.TrackingTarget = player.transform;
+                    return;
+                }
+            }
         }
     }
 }
